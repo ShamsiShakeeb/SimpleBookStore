f3f76d77deaf2dbcd2ef256662072ef52edd09d4
Washiq Anwar Shamsi <washiqshamsi@outlook.com>
2025-08-15 12:01:09 +0600

    Delete BLL Layer

--- SimpleBookStore.BLL/Services/ReviewService/IReviewService.cs
+++
@@ -1,9 +0,0 @@
-﻿//using SimpleBookStore.Model.Request;
-
-//namespace SimpleBookStore.BLL.Services.ReviewService
-//{
-//    public interface IReviewService
-//    {
-//        Task<(bool success, string message, string errorMessage)> GiveReview(ReviewRequestModel model);
-//    }
-//}
